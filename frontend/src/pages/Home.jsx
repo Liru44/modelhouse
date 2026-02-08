e0ef3f9c01dfd1060f102b1f2efc5578dfc77326
Liru44 <ant167@naver.com>
2026-02-08 23:55:40 +0900

    Initial commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1>OO 모델하우스</h1>
+            <p>프리미엄 라이프의 시작</p>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/main/java/com/modelhouse/ModelhouseApplication.java
@@ -0,0 +1,13 @@
+package com.modelhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelhouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModelhouseApplication.class, args);
+    }
+
+}
