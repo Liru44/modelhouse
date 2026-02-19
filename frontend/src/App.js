dc4c146fb727e36adb90c433d2acc62c0803b6b8
Liru44 <ant167@naver.com>
2026-02-20 02:22:55 +0900

    qna, image Table SQL Query 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,16 +5,16 @@ import Gallery from "./pages/Gallery";
 import Contact from "./pages/Contact";*/
 
 function App() {
-  return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/*<Route path="/location" element={<Location />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/contact" element={<Contact />} />*/}
-        </Routes>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+            <Route path="/" element={<Home />} />
+            {/*<Route path="/location" element={<Location />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/contact" element={<Contact />} />*/}
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
