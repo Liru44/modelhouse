2093a468fdca39c5c562ddb298e1f6d630ff308e
thkim8209 <hhh88261@gmail.com>
2026-03-10 18:21:20 +0900

    Header.jsx 수정 Header.css 생성

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,19 +1,57 @@
 import { Link } from "react-router-dom";
+import "../styles/Header.css";
+import { useState } from "react";
 
+// 헤더
 function Header() {
+  const [openIndex, setOpenIndex] = useState(null);
+
+  const menuData = [
+    {
+      title: "메뉴1",
+      subMenus: ["서브메뉴 1", "서브메뉴 2"],
+    },
+    {
+      title: "메뉴2",
+      subMenus: ["서브메뉴 1", "서브메뉴 2", "서브메뉴 3"],
+    },
+    {
+      title: "메뉴3",
+      subMenus: ["서브메뉴 1", "서브메뉴 2"],
+    },
+    {
+      title: "메뉴4",
+      subMenus: ["서브메뉴 1", "서브메뉴 2", "서브메뉴 3"],
+    },
+    {
+      title: "메뉴5",
+      subMenus: ["서브메뉴 1"],
+    },
+  ];
+
   return (
-    <header className="nav-bar">
-      <h1>Model house</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
+    <header>
+      <h1>로고</h1>
+      <ul id="header-menu">
+        {menuData.map((menu, index) => (
+          <li
+            key={index}
+            onMouseEnter={() => setOpenIndex(index)}
+            onMouseLeave={() => setOpenIndex(null)}
+          >
+            {menu.title} {/* 메뉴의 제목 (메뉴1, 메뉴2...) */}
+            {/* 현재 마우스가 올라간 메뉴의 index일 때만 서브메뉴 렌더링 */}
+            {openIndex === index && (
+              <ul className="dropdown">
+                {/* 각 메뉴가 가진 subMenus 배열을 다시 map으로 돌립니다 */}
+                {menu.subMenus.map((sub, subIndex) => (
+                  <li key={subIndex}>{sub}</li>
+                ))}
+              </ul>
+            )}
           </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
+        ))}
+      </ul>
     </header>
   );
 }
